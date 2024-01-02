b0887602a87653bc59740c2994f2a4eed9474353
Rob Bainbridge <bainbrid@users.noreply.github.com>
2024-01-02 12:05:55 +0000

    triv

--- scripts/sebastian_simple.py
+++ scripts/sebastian_simple.py
@@ -17,6 +17,8 @@ import matplotlib.pyplot as plt
 import ROOT as r 
 from setTDRStyle import setTDRStyle
 
+import pickle
+
 ################################################################################
 # I/O
 
@@ -27,7 +29,14 @@ from setTDRStyle import setTDRStyle
 # Input files
 filenames = [
     #"/eos/user/b/bainbrid/lowpteleid/nonres_large/output_000.root",
-    "../data/170823/nonres_large/output_000.root", # Rob's local version
+    #"../data/170823/nonres_large/output_0.root",   # Rob's small local version
+    #"../data/170823/nonres_large/output_000.root", # Rob's large local version
+    #"output/output_230816.root",
+    #"output/output_231011.root",
+    #"output/output_old.root",
+    #"output/output_new.root",
+    #"output/output.root",
+    "output/output_simple.root",
     ]
     
 columns = [
@@ -39,26 +48,36 @@ columns = [
 columns = list(set(columns))
 
 # Extract branches from root file as a pandas data frame
-df = [ uproot.open(i)['ntuplizer/tree'].arrays(columns,library="pd")  for i in filenames ]
-data = pd.concat(df)
+dfs = [ uproot.open(i)['ntuplizer/tree'].arrays(columns,library="pd")  for i in filenames ]
+df = pd.concat(dfs)
+print(df.describe(include='all').T)
 
 ################################################################################
 # Filters applied to branches
 ################################################################################
 
 # Filter data frame based on tag-side muon pT and eta
-tag_muon_pt = 7.0
+tag_muon_pt = 5.0
 tag_muon_eta = 1.5
-data = data[ (data.tag_pt>tag_muon_pt) & (np.abs(data.tag_eta)<tag_muon_eta) ]
+df = df[ (df.tag_pt>tag_muon_pt) & (np.abs(df.tag_eta)<tag_muon_eta) ]
 
 # Filter data frame for PF electrons (removing low-pT content)
-egamma = data[data.is_egamma]
+egamma = df[df.is_egamma]
 
 # Filter data frame keeping only PF electron candidates (removing tracks, etc)
+eta_upper = 2.5
 pt_lower = 2.0
-pt_upper = 5.0
-has_pfele = (egamma.has_ele) & (np.abs(egamma.ele_eta)<2.5) & (egamma.ele_pt>pt_lower)
-egamma = egamma[has_pfele]
+pt_upper = 1.e6
+#has_pfele = (egamma.has_ele) & (np.abs(egamma.ele_eta)<eta_upper) & (egamma.ele_pt>pt_lower)
+has_pfele  = ( (egamma.has_ele) & (np.abs(egamma.ele_eta)<2.5) )
+has_pfele &= ( (egamma.ele_pt>pt_lower) & (egamma.ele_pt<pt_upper) )
+
+print(pd.crosstab(
+    egamma.is_e,
+    [has_pfele],
+    rownames=['is_e'],
+    colnames=['has_pfele'],
+    margins=True))
 
 ################################################################################
 # ROC curve using matplotlib
@@ -72,11 +91,11 @@ xmin = 1.e-4
 plt.xlim(xmin,1.)
 plt.ylim([0., 1.])
 
-plt.xlabel(f"Mistag rate (w.r.t. reco'ed PF ele cand, pT > {pt_lower:.1f} GeV)")
-plt.ylabel(f"Efficiency (w.r.t. PF ele cand matched to truth, pT > {pt_lower:.1f} GeV)")
+plt.xlabel(f"Mistag rate (w.r.t. reco'ed PF ele cand, pT > {pt_lower:.1f} GeV, |eta| < {eta_upper})")
+plt.ylabel(f"Efficiency (w.r.t. PF ele cand matched to truth, pT > {pt_lower:.1f} GeV, |eta| < {eta_upper})")
 ax.tick_params(axis='x', pad=10.)
 plt.grid(True)
-#plt.gca().set_xscale('log')
+plt.gca().set_xscale('log')
 
 # "by chance" line
 plt.plot(
@@ -121,9 +140,18 @@ plt.plot(
     label='PF retrained ID ({:.3f})'.format(pf_id_retrain_auc)
     )
 
+# EXPORT PICKLE FILE WITH ROCS FROM SEBASTIAN SIMPLE SCRIPT !!!
+data = (
+    pf_id_retrain_fpr,pf_id_retrain_tpr,pf_id_retrain_thr,pf_id_retrain_auc, # Retrained
+    pf_id_fpr,pf_id_tpr,pf_id_thr,pf_id_auc # Original
+    )
+f = open('id_pf_retrain.pkl','wb')
+pickle.dump(data,f)
+
 plt.legend(loc='lower right',facecolor='white',framealpha=None,frameon=False)
 plt.tight_layout()
 plt.savefig('./roc_simple.pdf')
 plt.clf()
 plt.close()
 print('Created file: ./roc_simple.pdf')
+
